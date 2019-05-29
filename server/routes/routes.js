38c391894e026f8dd9926715b2ee22b3ba037e38
Mikkel Back Koll <217236@RTS-365.dk>
2019-05-29 10:56:16 +0200

    flyttede json objekt informationerne i mit route til variabler uden for de forskellige routes sådan at man nemmere kan tilgå dem i alle de routes man nu skulle bruge dem i. fik forklaret hvordan man sender mere end 1 array  med i et route.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,134 +1,74 @@
 module.exports = (app) => {
 
+   latestPostWidget =  [
+            {
+               "title": "Finances",
+               "image": "19.jpg",
+               "textContent": "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.",
+               "dateTime": "2019-05-14 07:00:00"
+            },
+            {
+               "title": "Politics",
+               "image": "20.jpg",
+               "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+               "dateTime": "2019-05-14 07:00:00"
+            },
+            {
+               "title": "Health",
+               "image": "21.jpg",
+               "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+               "dateTime": "2019-05-14 07:00:00"
+            },
+            {
+               "title": "Finance",
+               "image": "22.jpg",
+               "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+               "dateTime": "2019-05-14 07:00:00"
+            },
+            {
+               "title": "Travel",
+               "image": "23.jpg",
+               "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+               "dateTime": "2019-05-14 07:00:00"
+            },
+            {
+               "title": "Politics",
+               "image": "24.jpg",
+               "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+               "dateTime": "2019-05-14 07:00:00"
+            },
+         ]
+   newsSinglePost = [
+            {
+               "image":"12.jpg",
+               "title":"Finance",
+               "textContent":"Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet",
+               "likeImage":"like.png",
+               "chatImage":"chat.png",
+               "likeCount":"392",
+               "chatCount":"10"
+            }
+
+         ]
+   
+
    app.get('/', (req, res, next) => {
+      
       res.render('home', {
-         "latestPostWidgetData":
-            [
-               {
-                  "title": "Finances",
-                  "image": "19.jpg",
-                  "textContent": "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Politics",
-                  "image": "20.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Health",
-                  "image": "21.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Finance",
-                  "image": "22.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Travel",
-                  "image": "23.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Politics",
-                  "image": "24.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-            ]
+        "latestPostWidgetData":latestPostWidget,
+        "newsSinglePostData":newsSinglePost
       });
    });
 
    app.get('/categories', (req, res, next) => {
       res.render('categories', {
-         "latestPostWidgetData":
-            [
-               {
-                  "title": "Finances",
-                  "image": "19.jpg",
-                  "textContent": "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Politics",
-                  "image": "20.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Health",
-                  "image": "21.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Finance",
-                  "image": "22.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Travel",
-                  "image": "23.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Politics",
-                  "image": "24.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-            ]
+         "latestPostWidgetData":latestPostWidget
       });
    });
 
    app.get('/single-article', (req, res, next) => {
       res.render('single-article', {
-         "latestPostWidgetData":
-            [
-               {
-                  "title": "Finances",
-                  "image": "19.jpg",
-                  "textContent": "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Politics",
-                  "image": "20.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Health",
-                  "image": "21.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Finance",
-                  "image": "22.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Travel",
-                  "image": "23.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-               {
-                  "title": "Politics",
-                  "image": "24.jpg",
-                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "dateTime": "2019-05-14 07:00:00"
-               },
-            ]
+         "latestPostWidgetData":latestPostWidget
       });
    });
 
