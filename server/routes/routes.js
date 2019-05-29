78157b0e309366d09845a728db7dfa3301e792d2
Mikkel Back Koll <217236@RTS-365.dk>
2019-05-29 11:26:43 +0200

    Rykkede rundt på noget dato på latestPostWidget

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,37 +5,37 @@ module.exports = (app) => {
                "title": "Finances",
                "image": "19.jpg",
                "textContent": "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.",
-               "dateTime": "2019-05-14 07:00:00"
+               "dateTime": "April 14, 2019, 07:00:00"
             },
             {
                "title": "Politics",
                "image": "20.jpg",
                "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-               "dateTime": "2019-05-14 07:00:00"
+               "dateTime": "April 14, 2019, 07:00:00"
             },
             {
                "title": "Health",
                "image": "21.jpg",
                "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-               "dateTime": "2019-05-14 07:00:00"
+               "dateTime": "April 14, 2019, 07:00:00"
             },
             {
                "title": "Finance",
                "image": "22.jpg",
                "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-               "dateTime": "2019-05-14 07:00:00"
+               "dateTime": "April 14, 2019, 07:00:00"
             },
             {
                "title": "Travel",
                "image": "23.jpg",
                "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-               "dateTime": "2019-05-14 07:00:00"
+               "dateTime": "April 14, 2019, 07:00:00"
             },
             {
                "title": "Politics",
                "image": "24.jpg",
                "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-               "dateTime": "2019-05-14 07:00:00"
+               "dateTime": "April 14, 2019, 07:00:00"
             },
          ]
    newsSinglePost = [
