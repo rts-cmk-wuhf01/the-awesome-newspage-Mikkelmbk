41e6ee1e843a2442dbfee39e6cd4f4e18ddefdff
Mikkel Back Koll <217236@RTS-365.dk>
2019-05-29 09:23:18 +0200

    byggede latestPostWidget Sektionen i include filen via forEach på variabel fra Route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,15 +1,135 @@
 module.exports = (app) => {
 
    app.get('/', (req, res, next) => {
-      res.render('home');
+      res.render('home', {
+         "latestPostWidgetData":
+            [
+               {
+                  "title": "Finances",
+                  "image": "19.jpg",
+                  "textContent": "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Politics",
+                  "image": "20.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Health",
+                  "image": "21.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Finance",
+                  "image": "22.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Travel",
+                  "image": "23.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Politics",
+                  "image": "24.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+            ]
+      });
    });
 
    app.get('/categories', (req, res, next) => {
-      res.render('categories');
+      res.render('categories', {
+         "latestPostWidgetData":
+            [
+               {
+                  "title": "Finances",
+                  "image": "19.jpg",
+                  "textContent": "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Politics",
+                  "image": "20.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Health",
+                  "image": "21.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Finance",
+                  "image": "22.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Travel",
+                  "image": "23.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Politics",
+                  "image": "24.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+            ]
+      });
    });
 
    app.get('/single-article', (req, res, next) => {
-      res.render('single-article');
+      res.render('single-article', {
+         "latestPostWidgetData":
+            [
+               {
+                  "title": "Finances",
+                  "image": "19.jpg",
+                  "textContent": "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Politics",
+                  "image": "20.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Health",
+                  "image": "21.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Finance",
+                  "image": "22.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Travel",
+                  "image": "23.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+               {
+                  "title": "Politics",
+                  "image": "24.jpg",
+                  "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+                  "dateTime": "2019-05-14 07:00:00"
+               },
+            ]
+      });
    });
 
    app.get('/about-us', (req, res, next) => {
