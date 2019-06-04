05aacc50b41a62b25c37a6deb5a23367d67bdb9b
Mikkel Back Koll <217236@RTS-365.dk>
2019-06-04 13:42:51 +0200

    alle footer widgets har fået deres indhold fra objekter sendt via routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -147,35 +147,73 @@ const editorialMainPost = [
 const editorialSinglePost = [
    {
       "image":"7.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis dis parturient montes.",
+      "textContent":"Orci varius natoque penatibus et magnis",
       "date":"February 11, 2018"
    },
    {
       "image":"8.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis dis parturient montes.",
+      "textContent":"Orci varius natoque penatibus et magnis",
       "date":"February 11, 2018"
    },
    {
       "image":"9.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis dis parturient montes.",
+      "textContent":"Orci varius natoque penatibus et magnis",
       "date":"February 11, 2018"
    },
    {
       "image":"10.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis dis parturient montes.",
+      "textContent":"Orci varius natoque penatibus et magnis",
       "date":"February 11, 2018"
    },
    {
       "image":"11.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis dis parturient montes.",
-      "date":"February 11, 2018"
-   },
-   {
-      "image":"12.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis dis parturient montes.",
+      "textContent":"Orci varius natoque penatibus et magnis",
       "date":"February 11, 2018"
    }
 ]
+const footerContactWidget = {
+      "mail":"contact@youremail.com",
+      "phone":"+43 5278 2883 884",
+      "website":"www.yoursitename.com"
+   }
+const footerPoliticsWidget = {
+   "title":"Politics",
+   "business":"Business",
+   "markets":"Markets",
+   "tech":"Tech",
+   "luxury":"Luxury"
+}
+const footerFeaturedWidget = {
+   "title":"Featured",
+   "football":"Football",
+   "golf":"Golf",
+   "tennis":"Tennis",
+   "motorsport":"Motorsport",
+   "horseracing":"Horseracing",
+   "equestrian":"Equestrian",
+   "sailing":"Sailing",
+   "skiing":"Skiing"
+}
+const footerFAQWidget = {
+   "title":"FAQ",
+   "aviation":"Aviation",
+   "business":"Business",
+   "traveller":"Traveller",
+   "destinations":"Destinations",
+   "features":"Features",
+   "fooddrink":"Food/Drink",
+   "hotels":"Hotels",
+   "partnerhotels":"Partner Hotels"
+}
+const footerMoreWidget = {
+   "title":"+More",
+   "fashion":"Fashion",
+   "design":"Design",
+   "architecture":"Architecture",
+   "arts":"Arts",
+   "autos":"Autos",
+   "luxury":"Luxury"
+}
    app.get('/', (req, res, next) => {
       
       res.render('home', {
@@ -184,30 +222,57 @@ const editorialSinglePost = [
         "newsWidgetData":newsWidget,
         "videoData":videoContent,
         "editorialMainPostData":editorialMainPost,
-        "editorialSinglePostData":editorialSinglePost
+        "editorialSinglePostData":editorialSinglePost,
+        "footerContactWidgetData":footerContactWidget,
+        "footerPoliticsWidgetData":footerPoliticsWidget,
+        "footerFeaturedWidgetData":footerFeaturedWidget,
+        "footerFAQWidgetData":footerFAQWidget,
+        "footerMoreWidgetData":footerMoreWidget
       });
    });
 
    app.get('/categories', (req, res, next) => {
       res.render('categories', {
          "latestPostWidgetData":latestPostWidget,
-         "newsWidgetData":newsWidget
+         "newsWidgetData":newsWidget,
+         "footerContactWidgetData":footerContactWidget,
+         "footerPoliticsWidgetData":footerPoliticsWidget,
+         "footerFeaturedWidgetData":footerFeaturedWidget,
+         "footerFAQWidgetData":footerFAQWidget,
+         "footerMoreWidgetData":footerMoreWidget
       });
    });
 
    app.get('/single-article', (req, res, next) => {
       res.render('single-article', {
          "latestPostWidgetData":latestPostWidget,
-         "newsWidgetData":newsWidget
+         "newsWidgetData":newsWidget,
+         "footerContactWidgetData":footerContactWidget,
+         "footerPoliticsWidgetData":footerPoliticsWidget,
+         "footerFeaturedWidgetData":footerFeaturedWidget,
+         "footerFAQWidgetData":footerFAQWidget,
+         "footerMoreWidgetData":footerMoreWidget
       });
    });
 
    app.get('/about-us', (req, res, next) => {
-      res.render('about-us');
+      res.render('about-us',{
+         "footerContactWidgetData":footerContactWidget,
+         "footerPoliticsWidgetData":footerPoliticsWidget,
+         "footerFeaturedWidgetData":footerFeaturedWidget,
+         "footerFAQWidgetData":footerFAQWidget,
+         "footerMoreWidgetData":footerMoreWidget
+      });
    });
 
    app.get('/contact', (req, res, next) => {
-      res.render('contact');
+      res.render('contact',{
+         "footerContactWidgetData":footerContactWidget,
+         "footerPoliticsWidgetData":footerPoliticsWidget,
+         "footerFeaturedWidgetData":footerFeaturedWidget,
+         "footerFAQWidgetData":footerFAQWidget,
+         "footerMoreWidgetData":footerMoreWidget
+      });
    });
 
 };
