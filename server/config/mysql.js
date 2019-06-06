9a2656aa0c8056644cac7be93b60d9964079b2fa
Mikkel Back Koll <217236@RTS-365.dk>
2019-06-06 10:27:34 +0200

    1. lavede et database route der henter information fra min database ved navn testdatabasenewspage... 2. oprettede en testTemplate ejs fil til at udskrive informationen fra mn database på for at se om det virker som forventet... 3. rettede navne på mine Json objekter der bliver sendt med i mine routes så det er lowercase med underscores som ord seperatorer, JACKS LAW

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -1,13 +1,12 @@
-const mysql = require('mysql2'); // mysql
-
+const mysql = require('mysql2/promise'); // mysql
 module.exports = {
-   connect: function () {
-      return mysql.createConnection({
+   connect: async function () {
+      return await mysql.createConnection({
          host: 'localhost',
          user: 'root',
-         password: 'root',
-         port: '3306',
-         database: 'the_awesome_newpage'
+         password: '', // Hvis man ikke har sat et password selv skal plingerne være tomme indtil man selv har sat en password i phpmyadmin
+         port: '3306', // Dette er databasens port, (Kan også ses i Xampp ud for mysql under port kategorien), vores node servers port er ikke det samme som databasens port.
+         database: 'testdatabasenewspage'
       })
    }
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,303 +1,320 @@
+const mysql = require('../config/mysql');
+
 module.exports = (app) => {
 
-const latestPostWidget =  [
-            {
-               "title": "Finances",
-               "image": "19.jpg",
-               "textContent": "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.",
-               "dateTime": "April 14, 2019, 07:00:00"
-            },
-            {
-               "title": "Politics",
-               "image": "20.jpg",
-               "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-               "dateTime": "April 14, 2019, 07:00:00"
-            },
-            {
-               "title": "Health",
-               "image": "21.jpg",
-               "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-               "dateTime": "April 14, 2019, 07:00:00"
-            },
-            {
-               "title": "Finance",
-               "image": "22.jpg",
-               "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-               "dateTime": "April 14, 2019, 07:00:00"
-            },
-            {
-               "title": "Travel",
-               "image": "23.jpg",
-               "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-               "dateTime": "April 14, 2019, 07:00:00"
-            },
-            {
-               "title": "Politics",
-               "image": "24.jpg",
-               "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
-               "dateTime": "April 14, 2019, 07:00:00"
-            },
-         ]
-const newsSinglePost = [
-            {
-               "image":"12.jpg",
-               "title":"Finance",
-               "textContent":"Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet",
-               "likeImage":"like.png",
-               "chatImage":"chat.png",
-               "likeCount":"392",
-               "chatCount":"10"
-            },
-            {
-               "image":"13.jpg",
-               "title":"Finance",
-               "textContent":"Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet",
-               "likeImage":"like.png",
-               "chatImage":"chat.png",
-               "likeCount":"392",
-               "chatCount":"10"
-            },
-            {
-               "image":"14.jpg",
-               "title":"Finance",
-               "textContent":"Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet",
-               "likeImage":"like.png",
-               "chatImage":"chat.png",
-               "likeCount":"392",
-               "chatCount":"10"
-            },
-            {
-               "image":"15.jpg",
-               "title":"Finance",
-               "textContent":"Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet",
-               "likeImage":"like.png",
-               "chatImage":"chat.png",
-               "likeCount":"392",
-               "chatCount":"10"
-            },
-         ]
-const newsWidget = [
+   const latest_post_widget = [
       {
-         "newsWidgetDate":"April 14, 2018"
+         "title": "Finances",
+         "image": "19.jpg",
+         "textContent": "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.",
+         "dateTime": "April 14, 2019, 07:00:00"
       },
       {
-         "newsWidgetDate":"April 14, 2018"
+         "title": "Politics",
+         "image": "20.jpg",
+         "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+         "dateTime": "April 14, 2019, 07:00:00"
       },
       {
-         "newsWidgetDate":"April 14, 2018"
+         "title": "Health",
+         "image": "21.jpg",
+         "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+         "dateTime": "April 14, 2019, 07:00:00"
       },
       {
-         "newsWidgetDate":"April 14, 2018"
+         "title": "Finance",
+         "image": "22.jpg",
+         "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+         "dateTime": "April 14, 2019, 07:00:00"
       },
-]
-const videoContent = [
-     {
-        "image":"video1.jpg",
-        "videoLink":"https://www.youtube.com/watch?v=8u1eyw9p1hg"
-     },
-     {
-        "image":"video2.jpg",
-        "videoLink":"https://www.youtube.com/watch?v=1wiz0UsBPac"
-     },
-     {
-        "image":"video3.jpg",
-        "videoLink":"https://www.youtube.com/watch?v=sFYo2-KUeTA"
-     },
-     {
-        "image":"16.jpg",
-        "videoLink":"https://www.youtube.com/watch?v=Dd7FixvoKBw"
-     },
-     {
-        "image":"17.jpg",
-        "videoLink":"https://www.youtube.com/watch?v=WLAq3JVJ6Ho"
-     },
-  ]
-const editorialMainPost = [
-   {
-      "image":"1.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis dis parturient montes.",
-      "date":"February 11, 2018"
-   },
-   {
-      "image":"2.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis dis parturient montes.",
-      "date":"February 11, 2018"
-   },
-   {
-      "image":"3.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis dis parturient montes.",
-      "date":"February 11, 2018"
-   },
-   {
-      "image":"4.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis dis parturient montes.",
-      "date":"February 11, 2018"
-   },
-   {
-      "image":"5.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis dis parturient montes.",
-      "date":"February 11, 2018"
-   },
-   {
-      "image":"6.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis dis parturient montes.",
-      "date":"February 11, 2018"
+      {
+         "title": "Travel",
+         "image": "23.jpg",
+         "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+         "dateTime": "April 14, 2019, 07:00:00"
+      },
+      {
+         "title": "Politics",
+         "image": "24.jpg",
+         "textContent": "Sed a elit euismod augue semper congue sit amet ac sapien.",
+         "dateTime": "April 14, 2019, 07:00:00"
+      },
+   ]
+   const news_single_post = [
+      {
+         "image": "12.jpg",
+         "title": "Finance",
+         "textContent": "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet",
+         "likeImage": "like.png",
+         "chatImage": "chat.png",
+         "likeCount": "392",
+         "chatCount": "10"
+      },
+      {
+         "image": "13.jpg",
+         "title": "Finance",
+         "textContent": "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet",
+         "likeImage": "like.png",
+         "chatImage": "chat.png",
+         "likeCount": "392",
+         "chatCount": "10"
+      },
+      {
+         "image": "14.jpg",
+         "title": "Finance",
+         "textContent": "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet",
+         "likeImage": "like.png",
+         "chatImage": "chat.png",
+         "likeCount": "392",
+         "chatCount": "10"
+      },
+      {
+         "image": "15.jpg",
+         "title": "Finance",
+         "textContent": "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet",
+         "likeImage": "like.png",
+         "chatImage": "chat.png",
+         "likeCount": "392",
+         "chatCount": "10"
+      },
+   ]
+   const news_widget = [
+      {
+         "newsWidgetDate": "April 14, 2018"
+      },
+      {
+         "newsWidgetDate": "April 14, 2018"
+      },
+      {
+         "newsWidgetDate": "April 14, 2018"
+      },
+      {
+         "newsWidgetDate": "April 14, 2018"
+      },
+   ]
+   const video_content = [
+      {
+         "image": "video1.jpg",
+         "videoLink": "https://www.youtube.com/watch?v=8u1eyw9p1hg"
+      },
+      {
+         "image": "video2.jpg",
+         "videoLink": "https://www.youtube.com/watch?v=1wiz0UsBPac"
+      },
+      {
+         "image": "video3.jpg",
+         "videoLink": "https://www.youtube.com/watch?v=sFYo2-KUeTA"
+      },
+      {
+         "image": "16.jpg",
+         "videoLink": "https://www.youtube.com/watch?v=Dd7FixvoKBw"
+      },
+      {
+         "image": "17.jpg",
+         "videoLink": "https://www.youtube.com/watch?v=WLAq3JVJ6Ho"
+      },
+   ]
+   const editorial_main_post = [
+      {
+         "image": "1.jpg",
+         "textContent": "Orci varius natoque penatibus et magnis dis parturient montes.",
+         "date": "February 11, 2018"
+      },
+      {
+         "image": "2.jpg",
+         "textContent": "Orci varius natoque penatibus et magnis dis parturient montes.",
+         "date": "February 11, 2018"
+      },
+      {
+         "image": "3.jpg",
+         "textContent": "Orci varius natoque penatibus et magnis dis parturient montes.",
+         "date": "February 11, 2018"
+      },
+      {
+         "image": "4.jpg",
+         "textContent": "Orci varius natoque penatibus et magnis dis parturient montes.",
+         "date": "February 11, 2018"
+      },
+      {
+         "image": "5.jpg",
+         "textContent": "Orci varius natoque penatibus et magnis dis parturient montes.",
+         "date": "February 11, 2018"
+      },
+      {
+         "image": "6.jpg",
+         "textContent": "Orci varius natoque penatibus et magnis dis parturient montes.",
+         "date": "February 11, 2018"
+      }
+   ]
+   const editorial_single_post = [
+      {
+         "image": "7.jpg",
+         "textContent": "Orci varius natoque penatibus et magnis",
+         "date": "February 11, 2018"
+      },
+      {
+         "image": "8.jpg",
+         "textContent": "Orci varius natoque penatibus et magnis",
+         "date": "February 11, 2018"
+      },
+      {
+         "image": "9.jpg",
+         "textContent": "Orci varius natoque penatibus et magnis",
+         "date": "February 11, 2018"
+      },
+      {
+         "image": "10.jpg",
+         "textContent": "Orci varius natoque penatibus et magnis",
+         "date": "February 11, 2018"
+      },
+      {
+         "image": "11.jpg",
+         "textContent": "Orci varius natoque penatibus et magnis",
+         "date": "February 11, 2018"
+      }
+   ]
+   const footer_contact_widget = {
+      "mail": "contact@youremail.com",
+      "phone": "+43 5278 2883 884",
+      "website": "www.yoursitename.com"
    }
-]
-const editorialSinglePost = [
-   {
-      "image":"7.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis",
-      "date":"February 11, 2018"
-   },
-   {
-      "image":"8.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis",
-      "date":"February 11, 2018"
-   },
-   {
-      "image":"9.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis",
-      "date":"February 11, 2018"
-   },
-   {
-      "image":"10.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis",
-      "date":"February 11, 2018"
-   },
-   {
-      "image":"11.jpg",
-      "textContent":"Orci varius natoque penatibus et magnis",
-      "date":"February 11, 2018"
+   const footer_politics_widget = {
+      "title": "Politics",
+      "business": "Business",
+      "markets": "Markets",
+      "tech": "Tech",
+      "luxury": "Luxury"
    }
-]
-const footerContactWidget = {
-      "mail":"contact@youremail.com",
-      "phone":"+43 5278 2883 884",
-      "website":"www.yoursitename.com"
+   const footer_featured_widget = {
+      "title": "Featured",
+      "football": "Football",
+      "golf": "Golf",
+      "tennis": "Tennis",
+      "motorsport": "Motorsport",
+      "horseracing": "Horseracing",
+      "equestrian": "Equestrian",
+      "sailing": "Sailing",
+      "skiing": "Skiing"
    }
-const footerPoliticsWidget = {
-   "title":"Politics",
-   "business":"Business",
-   "markets":"Markets",
-   "tech":"Tech",
-   "luxury":"Luxury"
-}
-const footerFeaturedWidget = {
-   "title":"Featured",
-   "football":"Football",
-   "golf":"Golf",
-   "tennis":"Tennis",
-   "motorsport":"Motorsport",
-   "horseracing":"Horseracing",
-   "equestrian":"Equestrian",
-   "sailing":"Sailing",
-   "skiing":"Skiing"
-}
-const footerFAQWidget = {
-   "title":"FAQ",
-   "aviation":"Aviation",
-   "business":"Business",
-   "traveller":"Traveller",
-   "destinations":"Destinations",
-   "features":"Features",
-   "fooddrink":"Food/Drink",
-   "hotels":"Hotels",
-   "partnerhotels":"Partner Hotels"
-}
-const footerMoreWidget = {
-   "title":"+More",
-   "fashion":"Fashion",
-   "design":"Design",
-   "architecture":"Architecture",
-   "arts":"Arts",
-   "autos":"Autos",
-   "luxury":"Luxury"
-}
-const singleArticleCommentArea = [
-   {
-      "author":"Tim-Othy",
-      "image":"30.jpg",
-      "date":"April 15, 2018",
-      "commentText":"Donec turpis erat, scelerisque id euismod sit amet, fermentum vel dolor. Nulla facilisi. Sed"
-   },
-   {
-      "author":"A-aron",
-      "image":"30.jpg",
-      "date":"April 15, 2018",
-      "commentText":"Donec turpis erat, scelerisque id euismod sit amet, fermentum vel dolor. Nulla facilisi. Sed"
-   },
-   {
-      "author":"B-LAKE",
-      "image":"31.jpg",
-      "date":"April 15, 2018",
-      "commentText":"Donec turpis erat, scelerisque id euismod sit amet, fermentum vel dolor. Nulla facilisi. Sed"
-   },
-   {
-      "author":"DEE-NICE",
-      "image":"32.jpg",
-      "date":"April 15, 2018",
-      "commentText":"Donec turpis erat, scelerisque id euismod sit amet, fermentum vel dolor. Nulla facilisi. Sed"
-   },
-]
+   const footer_fAQ_widget = {
+      "title": "FAQ",
+      "aviation": "Aviation",
+      "business": "Business",
+      "traveller": "Traveller",
+      "destinations": "Destinations",
+      "features": "Features",
+      "fooddrink": "Food/Drink",
+      "hotels": "Hotels",
+      "partnerhotels": "Partner Hotels"
+   }
+   const footer_more_widget = {
+      "title": "+More",
+      "fashion": "Fashion",
+      "design": "Design",
+      "architecture": "Architecture",
+      "arts": "Arts",
+      "autos": "Autos",
+      "luxury": "Luxury"
+   }
+   const single_article_comment_area = [
+      {
+         "author": "Tim-Othy",
+         "image": "30.jpg",
+         "date": "April 15, 2018",
+         "commentText": "Donec turpis erat, scelerisque id euismod sit amet, fermentum vel dolor. Nulla facilisi. Sed"
+      },
+      {
+         "author": "A-aron",
+         "image": "30.jpg",
+         "date": "April 15, 2018",
+         "commentText": "Donec turpis erat, scelerisque id euismod sit amet, fermentum vel dolor. Nulla facilisi. Sed"
+      },
+      {
+         "author": "B-LAKE",
+         "image": "31.jpg",
+         "date": "April 15, 2018",
+         "commentText": "Donec turpis erat, scelerisque id euismod sit amet, fermentum vel dolor. Nulla facilisi. Sed"
+      },
+      {
+         "author": "DEE-NICE",
+         "image": "32.jpg",
+         "date": "April 15, 2018",
+         "commentText": "Donec turpis erat, scelerisque id euismod sit amet, fermentum vel dolor. Nulla facilisi. Sed"
+      },
+   ]
+
+   app.get('/database', async (req, res, next) => {
+      let db = await mysql.connect();
+      let [products] = await db.execute('SELECT * FROM products');
+      db.end();
+
+      res.render('testTemplate.ejs', {
+         'products': products
+      });
+
+   });
+
+
    app.get('/', (req, res, next) => {
       res.render('home', {
-        "latestPostWidgetData":latestPostWidget,
-        "newsSinglePostData":newsSinglePost,
-        "newsWidgetData":newsWidget,
-        "videoData":videoContent,
-        "editorialMainPostData":editorialMainPost,
-        "editorialSinglePostData":editorialSinglePost,
-        "footerContactWidgetData":footerContactWidget,
-        "footerPoliticsWidgetData":footerPoliticsWidget,
-        "footerFeaturedWidgetData":footerFeaturedWidget,
-        "footerFAQWidgetData":footerFAQWidget,
-        "footerMoreWidgetData":footerMoreWidget
+         "latestPostWidgetData": latest_post_widget,
+         "newsSinglePostData": news_single_post,
+         "newsWidgetData": news_widget,
+         "videoData": video_content,
+         "editorialMainPostData": editorial_main_post,
+         "editorialSinglePostData": editorial_single_post,
+         "footerContactWidgetData": footer_contact_widget,
+         "footerPoliticsWidgetData": footer_politics_widget,
+         "footerFeaturedWidgetData": footer_featured_widget,
+         "footerFAQWidgetData": footer_fAQ_widget,
+         "footerMoreWidgetData": footer_more_widget
       });
    });
 
+
+
    app.get('/categories', (req, res, next) => {
       res.render('categories', {
-         "latestPostWidgetData":latestPostWidget,
-         "newsWidgetData":newsWidget,
-         "footerContactWidgetData":footerContactWidget,
-         "footerPoliticsWidgetData":footerPoliticsWidget,
-         "footerFeaturedWidgetData":footerFeaturedWidget,
-         "footerFAQWidgetData":footerFAQWidget,
-         "footerMoreWidgetData":footerMoreWidget
+         "latestPostWidgetData": latest_post_widget,
+         "newsWidgetData": news_widget,
+         "footerContactWidgetData": footer_contact_widget,
+         "footerPoliticsWidgetData": footer_politics_widget,
+         "footerFeaturedWidgetData": footer_featured_widget,
+         "footerFAQWidgetData": footer_fAQ_widget,
+         "footerMoreWidgetData": footer_more_widget
       });
    });
 
    app.get('/single-article', (req, res, next) => {
       res.render('single-article', {
-         "latestPostWidgetData":latestPostWidget,
-         "newsWidgetData":newsWidget,
-         "footerContactWidgetData":footerContactWidget,
-         "footerPoliticsWidgetData":footerPoliticsWidget,
-         "footerFeaturedWidgetData":footerFeaturedWidget,
-         "footerFAQWidgetData":footerFAQWidget,
-         "footerMoreWidgetData":footerMoreWidget,
-         "singleArticleCommentAreaData":singleArticleCommentArea
+         "latestPostWidgetData": latest_post_widget,
+         "newsWidgetData": news_widget,
+         "footerContactWidgetData": footer_contact_widget,
+         "footerPoliticsWidgetData": footer_politics_widget,
+         "footerFeaturedWidgetData": footer_featured_widget,
+         "footerFAQWidgetData": footer_fAQ_widget,
+         "footerMoreWidgetData": footer_more_widget,
+         "singleArticleCommentAreaData": single_article_comment_area
       });
    });
 
    app.get('/about-us', (req, res, next) => {
-      res.render('about-us',{
-         "footerContactWidgetData":footerContactWidget,
-         "footerPoliticsWidgetData":footerPoliticsWidget,
-         "footerFeaturedWidgetData":footerFeaturedWidget,
-         "footerFAQWidgetData":footerFAQWidget,
-         "footerMoreWidgetData":footerMoreWidget
+      res.render('about-us', {
+         "footerContactWidgetData": footer_contact_widget,
+         "footerPoliticsWidgetData": footer_politics_widget,
+         "footerFeaturedWidgetData": footer_featured_widget,
+         "footerFAQWidgetData": footer_fAQ_widget,
+         "footerMoreWidgetData": footer_more_widget
       });
    });
 
    app.get('/contact', (req, res, next) => {
-      res.render('contact',{
-         "footerContactWidgetData":footerContactWidget,
-         "footerPoliticsWidgetData":footerPoliticsWidget,
-         "footerFeaturedWidgetData":footerFeaturedWidget,
-         "footerFAQWidgetData":footerFAQWidget,
-         "footerMoreWidgetData":footerMoreWidget
+      res.render('contact', {
+         "footerContactWidgetData": footer_contact_widget,
+         "footerPoliticsWidgetData": footer_politics_widget,
+         "footerFeaturedWidgetData": footer_featured_widget,
+         "footerFAQWidgetData": footer_fAQ_widget,
+         "footerMoreWidgetData": footer_more_widget
       });
    });
 
