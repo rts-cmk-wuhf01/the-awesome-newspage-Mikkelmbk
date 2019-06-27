238601dc7505897bc054a3555fb64286a12075cc
Mikkel Back Koll <217236@RTS-365.dk>
2019-06-27 09:31:09 +0200

    Med hjælp fra jack fik jeg min UPDATE sql til at virke, det viser sig at man bruger samme kode som når man skal indsætte noget i databasen, bortset fra at man istedet for at skrive INSERT INTO så skriver UPDATE

--- server/routes/admin_categories.js
+++ server/routes/admin_categories.js
@@ -44,15 +44,15 @@ module.exports = app => {
 		   let db = await mysql.connect();
 		   let categories = await getCategoriesWithoutLimit();
   
-		   let result = await db.execute(`
+		   let [result] = await db.execute(`
 			INSERT INTO categories
 			SET
 			category_title = ?`,
 			[title]);
   
-		   if (result[0].affectedRows > 0) { // hvis der er mere end 1 række der er blevet påvirket i databasen (altså at den har modtaget noget) så kør if statement koden
+		   if (result.affectedRows > 0) { // hvis der er mere end 1 række der er blevet påvirket i databasen (altså at den har modtaget noget) så kør if statement koden
 			  // console.log(affectedRows);
-			  res.redirect('/admin_categories'); // omdirriger til /contact siden men med ?success på.
+			  res.redirect('/admin/categories'); // omdirriger til /contact siden men med ?success på.
 		   }
 		   else {
 			  res.render('admin_categories', {
@@ -81,6 +81,57 @@ module.exports = app => {
   });
 
   app.post("/admin/categories/edit/:category_id", async (req, res, next) => {
+	
+
+		
+	let title = req.body.category_title;
+  
+	let return_message = [];
+
+	if (typeof title == 'undefined' || title == '' || !isNaN(title)) {
+	   return_message.push('Hov, der gik noget galt med din title');
+	};
+
+
+	if (return_message.length > 0) { // hvis arrayet return_message's længde er større end 0
+	   let db = await mysql.connect();
+	   let categories = await getCategoriesWithoutLimit();
+
+	   db.end();
+	   res.render('admin_categories', {
+		'categoriesData':categories,
+		"returnMessageData": return_message.join(', '), // tag hvert index i arrayet og placer dem i en samlet string, men med komma og mellemrum imellem hver af dem.
+		"valuesData": req.body // er en del af body-parser modulet, body her har intet med html body tagget at gøre.
+	   });
+
+	}
+	else {
+
+	   return_message = "Tak for din Besked vi vender tilbage hurtigst muligt";
+	   let db = await mysql.connect();
+	   let categories = await getCategoriesWithoutLimit();
+
+	   let [result] = await db.execute(`
+		UPDATE categories
+		SET
+		category_title = ?
+		WHERE category_id = ?`,
+		[title,req.params.category_id]);
+
+	   if (result.affectedRows > 0) { // hvis der er mere end 1 række der er blevet påvirket i databasen (altså at den har modtaget noget) så kør if statement koden
+		  // console.log(affectedRows);
+		  res.redirect('/admin/categories'); // omdirriger til /contact siden men med ?success på.
+	   }
+	   else {
+		  res.render('admin_categories', {
+			 "categoriesData":categories,
+			 "returnMessageData": return_message,
+			 "valuesData": []
+		  });
+	   }
+	   db.end();
+
+	}
 	
   });
 
